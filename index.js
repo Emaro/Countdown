483b70515d3b53a8970e8a6a6f7acbc5458c5687
Emaro <Emaro@users.noreply.github.com>
2017-10-14 22:04:01 +0200

    Countdown 0.2 (#11)
    * Add Dev to H1
    
    * Change default time to one minute
    
    * Add ajaxServer test script
    
    * Add ajaxClient
    
    * Add ajax loop
    
    * Add ajaxServer and ajaxClient to gitignore
    
    * Try notification
    
    * Update
    
    * Add desktop notification
    
    * Add icon to notification
    
    * Close #3
    
    * Add inline styling
    
    * Refactor css and js
    
    * Small refactoring
    
    * Revert "Merge branch 'master' into dev"
    
    This reverts commit b0e38a7a6066cf1556b53831d11041bbd2d9f894, reversing
    changes made to 6d0816b4857a8fea927e45ad10eb37e88c143203.

---
+++ index.js
@@ -0,0 +1,96 @@
+var timer
+
+function startCountdown() {
+
+    clearInterval(timer)
+
+    var ms
+    ms = getAsNumber("hh") * 60 * 60 * 1000
+    ms += getAsNumber("mm") * 60 * 1000;
+    ms += getAsNumber("ss") * 1000;
+
+    var now = new Date().getTime()
+
+    var targetTimeInMs = now + ms
+
+    var targetTime = new Date(targetTimeInMs);
+
+    countToZero(targetTime, timer);
+
+    timer = setInterval(countToZero, 100, targetTime, timer);
+}
+
+function getAsNumber(id) {
+    var i = parseInt(getValue(id))
+    return isNaN(i)
+        ? 0
+        : i
+}
+
+function getValue(id) {
+    return getElement(id).value
+}
+function countToZero(targetTime, timer) {
+
+    var dif = msLeftUntil(targetTime)
+
+    if (dif <= 100) {
+        getElement("output").innerHTML = getTimeString(0, 0, 0)
+        notifyMe();
+        clearInterval(timer);
+        return
+    }
+
+    dif /= 1000; // s
+    var sec = Math.floor(dif % 60);
+    dif /= 60; // m
+    var min = Math.floor(dif % 60);
+    dif /= 60 // h;
+    var hour = Math.floor(dif);
+
+    getElement("output").innerHTML = getTimeString(hour, min, sec)
+}
+
+function msLeftUntil(targetTime) {
+    return targetTime - new Date()
+}
+
+function getTimeString(h, m, s) {
+    return pad(h) + ":" + pad(m) + ":" + pad(s)
+}
+
+function pad(n) {
+    return (n < 10) ? ("0" + n) : n
+}
+
+function getElement(id) {
+    return document.getElementById(id)
+}
+
+// request permission on page load
+document.addEventListener('DOMContentLoaded', function () {
+    if (!Notification) {
+        alert('Desktop notifications not available in your browser.')
+        return
+    }
+
+    if (Notification.permission !== "granted")
+        Notification.requestPermission()
+});
+
+function notifyMe() {
+    if (Notification.permission !== "granted")
+    // Notification.requestPermission();
+        alert("Countdown finished")
+    else {
+
+        var notification = new Notification('Countdown finished', {
+            icon: './favicon-96x96.png',
+            body: "",
+        });
+
+        notification.onclick = function () {
+            window.open("")
+        }
+    }
+}
